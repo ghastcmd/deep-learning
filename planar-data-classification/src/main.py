de5bc436a402f986e9ba2eeaa6bb19581e5b07f3
GhastCMD <kraken32bin@protonmail.com>
2021-07-24 10:30:57 -0300

    Completed the chapter 2 and 3

--- planar-data-classification/src/main.py
+++ planar-data-classification/src/main.py
@@ -9,4 +9,29 @@ from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, l
 
 np.random.seed(1) # set a seed so that the results are consistent
 
-print('Hello world')
+X, Y = load_planar_dataset()
+
+# Visualize the data:
+plt.scatter(X[0, :], X[1, :])#, c=Y, s=40, cmap=plt.cm.Spectral);
+plt.xlabel('x')
+plt.ylabel('y')
+
+### START CODE HERE ### (≈ 3 lines of code)
+shape_X = X.shape
+shape_Y = Y.shape
+m = Y.shape[1]  # training set size
+### END CODE HERE ###
+
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
+
+clf = sklearn.linear_model.LogisticRegressionCV();
+clf.fit(X.T, Y.ravel())
+
+plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+plt.title('Logistic Regression')
+
+LR_predictions = clf.predict(X.T)
+print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+       '% ' + "(percentage of correctly labelled datapoints)")
