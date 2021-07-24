16b9f25f3a912f56c947650413777e848a5dae2e
GhastCMD <kraken32bin@protonmail.com>
2021-07-24 10:55:18 -0300

    Completed the chapter 4.3

--- planar-data-classification/src/main.py
+++ planar-data-classification/src/main.py
@@ -35,3 +35,161 @@ plt.title('Logistic Regression')
 LR_predictions = clf.predict(X.T)
 print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
        '% ' + "(percentage of correctly labelled datapoints)")
+
+# GRADED FUNCTION: layer_sizes
+
+def layer_sizes(X, Y):
+    """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+    
+    Returns:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+    ### START CODE HERE ### (≈ 3 lines of code)
+    n_x = X.shape[0] # size of input layer
+    n_h = 4
+    n_y = Y.shape[0] # size of output layer
+    ### END CODE HERE ###
+    return (n_x, n_h, n_y)
+
+X_assess, Y_assess = layer_sizes_test_case()
+(n_x, n_h, n_y) = layer_sizes(X_assess, Y_assess)
+print("The size of the input layer is: n_x = " + str(n_x))
+print("The size of the hidden layer is: n_h = " + str(n_h))
+print("The size of the output layer is: n_y = " + str(n_y))
+
+# GRADED FUNCTION: initialize_parameters
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    
+    np.random.seed(2) # we set up a seed so that your output matches ours although the initialization is random.
+    
+    ### START CODE HERE ### (≈ 4 lines of code)
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    ### END CODE HERE ###
+    
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+n_x, n_h, n_y = initialize_parameters_test_case()
+
+parameters = initialize_parameters(n_x, n_h, n_y)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+# GRADED FUNCTION: forward_propagation
+
+def forward_propagation(X, parameters):
+    """
+    Argument:
+    X -- input data of size (n_x, m)
+    parameters -- python dictionary containing your parameters (output of initialization function)
+    
+    Returns:
+    A2 -- The sigmoid output of the second activation
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+    """
+    # Retrieve each parameter from the dictionary "parameters"
+    ### START CODE HERE ### (≈ 4 lines of code)
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    ### END CODE HERE ###
+    
+    # Implement Forward Propagation to calculate A2 (probabilities)
+    ### START CODE HERE ### (≈ 4 lines of code)
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    ### END CODE HERE ###
+    
+    assert(A2.shape == (1, X.shape[1]))
+    
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    
+    return A2, cache
+
+X_assess, parameters = forward_propagation_test_case()
+A2, cache = forward_propagation(X_assess, parameters)
+
+# Note: we use the mean here just to make sure that your output matches ours. 
+print(np.mean(cache['Z1']) ,np.mean(cache['A1']),np.mean(cache['Z2']),np.mean(cache['A2']))
+
+print("\nX_assess = \n", X_assess)
+print("\nA2 = \n", A2)
+
+# GRADED FUNCTION: compute_cost
+
+def compute_cost(A2, Y, parameters):
+    """
+    Computes the cross-entropy cost given in equation (13)
+    
+    Arguments:
+    A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+    parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+    [Note that the parameters argument is not used in this function, 
+    but the auto-grader currently expects this parameter.
+    Future version of this notebook will fix both the notebook 
+    and the auto-grader so that `parameters` is not needed.
+    For now, please include `parameters` in the function signature,
+    and also when invoking this function.]
+    
+    Returns:
+    cost -- cross-entropy cost given equation (13)
+    
+    """
+    
+    m = Y.shape[1] # number of example
+
+    # Compute the cross-entropy cost
+    ### START CODE HERE ### (≈ 2 lines of code)
+    logprobs = Y * np.log(A2) + ~Y * np.log(1 - A2)
+    cost = - (1/m) * logprobs.sum()
+    ### END CODE HERE ###
+    
+    cost = float(np.squeeze(cost))  # makes sure cost is the dimension we expect. 
+                                    # E.g., turns [[17]] into 17 
+    assert(isinstance(cost, float))
+    
+    return cost
+
+A2, Y_assess, parameters = compute_cost_test_case()
+
+print("cost = " + str(compute_cost(A2, Y_assess, parameters)))
